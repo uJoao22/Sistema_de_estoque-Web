9b3212e25a0d944193cb003ce21b5388959ab09b
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-05-10 20:28:40 -0300

    Adicionando condiguraçõs Back e Json

---
+++ Projeto/Back-End/sistemaestoque/src/main/java/br/com/eletroandrade/model/Produto.java
@@ -0,0 +1,121 @@
+package br.com.eletroandrade.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	private Long codigo;
+	private String nome;
+	private Categoria categoria;
+	private double preco;
+	private int quantidade;
+	private String marca;
+	private String modelo;
+	private String adicionais;
+	
+	public Produto() {
+	}
+	
+	public Produto(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	@Id
+	@Column(name = "codigo_produto")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	@NotNull
+	@Size(max = 100)
+	@Column(name = "nome", nullable = false)
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@NotNull
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria", nullable = false)
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@NotNull
+	@Range(min=0)
+	@Column(name = "preco", columnDefinition="numeric(10,2) default 0.00", nullable = false)
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@NotNull
+	@Column(name = "quantidade", nullable = false)
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Size(max = 30)
+	@Column(name = "marca")
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Size(max = 100)
+	@Column(name = "modelo")
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	@Column(name = "adicionais")
+	@Type(type = "org.hibernate.type.TextType")
+	public String getAdicionais() {
+		return adicionais;
+	}
+
+	public void setAdicionais(String adicionais) {
+		this.adicionais = adicionais;
+	}
+	
+}
