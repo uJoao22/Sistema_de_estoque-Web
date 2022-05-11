e9cf21db04cd992a14618a68a54da4cd508fa06c
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-05-10 23:13:11 -0300

    Sprint 2

--- Projeto/Banco/script_criacao_database.sql
+++ Projeto/Banco/script_criacao_database.sql
@@ -46,7 +46,7 @@ DROP TABLE IF EXISTS `produtos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produtos` (
-  `codigo_produto` int NOT NULL,
+  `codigo_produto` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `id_categoria` int NOT NULL,
   `preco` decimal(10,0) NOT NULL,
