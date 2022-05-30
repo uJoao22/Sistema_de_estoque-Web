e66d1532c565a490ae14fb57f25ca1507deed53e
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-05-30 19:53:01 -0300

    Corrigindo problema de cors

--- Projeto/Front-End/src/modules/estoque/script.js
+++ Projeto/Front-End/src/modules/estoque/script.js
@@ -1,2 +1,8 @@
 $("#NavBar").load("../../components/NavBar/NavBar.html");
 $("#Footer").load("../../components/Footer/Footer.html");
+
+$(document).ready(() => {
+    $.get('http://localhost:8082/sistemaestoque/produtos', (data) =>{
+        console.log(data);
+    });
+});
--- Projeto/Front-End/src/script.js
+++ Projeto/Front-End/src/script.js
@@ -3,24 +3,9 @@ $("#Footer").load("./components/Footer/Footer.html");
 
 document.querySelector("#login").addEventListener("click", () => {
     window.location.assign("./modules/menu/index.html");
+    // $.get('http://localhost:8082/sistemaestoque/produtos', (data) =>{
+    //     console.log(data);
+    // });
 });
 
-// Access-Control-Allow-Origin: *;
 
-$.ajax({
-    methot: 'GET',
-    url: 'http://127.0.0.1:8082/sistemaestoque/produtos',
-    dataType: 'json',
-    // headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     contentType: 'application/json',
-    // }
-}).done((data) =>{
-    console.log(data);
-}).fail((erro) => {
-    console.log(erro);
-});
-
-// $.get('http://localhost:8082/sistemaestoque/produtos', (data) =>{
-//     console.log(data);
-// });
